fad726c44ad6e2b0efeda37cd2c21e0a5e9f48ee
Pedro Ferreira <83607190+pedrodif@users.noreply.github.com>
2024-06-19 18:22:33 +0000

    feat/ adiciona struct quarto, procedimento cadastrarQuarto, contante MAX_QUARTOS

--- .ccls-cache/@home@runner@uploadSistemaHotel/main.c
+++ .ccls-cache/@home@runner@uploadSistemaHotel/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_CLIENTES 100
 #define MAX_FUNCIONARIOS 50
+#define MAX_QUARTOS 100
 
 typedef struct {
   int codigo;
@@ -17,6 +19,13 @@ typedef struct {
   char cargo[50];
   float salario;
 } Funcionario;
+
+typedef struct {
+  int numero;
+  int quantidade_hospedes;
+  float valor_diaria;
+  char status[10];
+} Quarto;
 
 void cadastrarCliente(Cliente clientes[], int *num_clientes) {
   if (*num_clientes >= MAX_CLIENTES) {
@@ -81,11 +90,42 @@ void cadastrarFuncionario(Funcionario funcionarios[], int *num_funcionarios) {
   printf("Funcionário cadastrado com sucesso.\n");
 }
 
+void cadastrarQuarto(Quarto quartos[], int *num_quartos) {
+  if (*num_quartos >= MAX_QUARTOS) {
+    printf("Limite de quartos atingido.\n");
+    return;
+  }
+
+  Quarto novo_quarto;
+
+  printf("Número do quarto: ");
+  scanf("%d", &novo_quarto.numero);
+
+  printf("Quantidade de hóspedes: ");
+  scanf("%d", &novo_quarto.quantidade_hospedes);
+
+  printf("Valor da diária: ");
+  scanf("%f", &novo_quarto.valor_diaria);
+
+  strcpy(novo_quarto.status, "desocupado");
+
+  for (int i = 0; i < *num_quartos; i++) {
+    if (quartos[i].numero == novo_quarto.numero) {
+      printf("Erro: já existe um quarto com esse número.\n");
+      return;
+    }
+  }
+
+  quartos[*num_quartos] = novo_quarto;
+  (*num_quartos)++;
+  printf("Quarto cadastrado com sucesso.\n");
+}
+
 int main(void) {
+  Quarto quartos[MAX_QUARTOS];
   Cliente clientes[MAX_CLIENTES];
   Funcionario funcionarios[MAX_FUNCIONARIOS];
 
-  int num_clientes = 0, num_funcionarios = 0;
-
+  int num_clientes = 0, num_funcionarios = 0, num_quartos = 0;
   return 0;
 }
